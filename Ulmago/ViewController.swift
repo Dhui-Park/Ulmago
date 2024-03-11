192b4d507645c56e630b9997bce23e2b5012ac65
dhuiPark <hasong925@gmail.com>
2024-03-11 22:39:28 +0900

    goalSettingView UI

--- Ulmago/ViewController.swift
+++ Ulmago/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var goalTextField: UITextField!
+    
+    @IBOutlet weak var submitBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.goalTextField.placeholder = "맥북 프로 / 괌 여행 / 자전거"
     }
 
-
+    @IBAction func submitBtnClicked(_ sender: UIButton) {
+        print(#fileID, #function, #line, "- ")
+    }
+    
 }
 
