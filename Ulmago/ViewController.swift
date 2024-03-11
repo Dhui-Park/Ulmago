e41e842dc08b2c2c458fa8f925e99d50576b302b
dhuiPark <hasong925@gmail.com>
2024-03-11 21:20:08 +0900

    Initial Commit

---
+++ Ulmago/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ulmago
+//
+//  Created by dhui on 3/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
