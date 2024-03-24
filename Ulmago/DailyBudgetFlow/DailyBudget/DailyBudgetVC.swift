5e75347211bcd9789aad878f0778c2fbdcc61be9
dhuiPark <hasong925@gmail.com>
2024-03-25 02:21:47 +0900

    DailyBudgetVC - custom alert view

--- Ulmago/DailyBudgetFlow/DailyBudget/DailyBudgetVC.swift
+++ Ulmago/DailyBudgetFlow/DailyBudget/DailyBudgetVC.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import SwiftAlertView
 
 class DailyBudgetVC: UIViewController {
     
@@ -95,49 +96,100 @@ class DailyBudgetVC: UIViewController {
     
     @IBAction func addBtnClicked(_ sender: UIButton) {
         print(#fileID, #function, #line, "- ")
-        let alertController = UIAlertController(title: "추가하시겠습니까?", message: "무엇에 얼마를 썼나요?", preferredStyle: .alert)
-        
-        
-        // add textfield at index 0
-        alertController.addTextField(configurationHandler: {(_ textField: UITextField) -> Void in
-            textField.placeholder = "내역"
-            
-        })
-        
-        // add textfield at index 1
-        alertController.addTextField(configurationHandler: {(_ textField: UITextField) -> Void in
-            textField.placeholder = "금액"
-            textField.keyboardType = .numberPad
-            textField.delegate = self
-        })
-        
-        // Alert action confirm
-        let confirmAction = UIAlertAction(title: "OK", style: .default, handler: {(_ action: UIAlertAction) -> Void in
-            
-            guard let newTitle = alertController.textFields?[0].text,
-                  let newPrice = alertController.textFields?[1].text else { return }
-            
-            
-            print("내역: \(newTitle)")
-            print("금액: \(newPrice)")
-            
-            DispatchQueue.main.async {
-                self.budgetList.insert(Budget(title: newTitle, price: Int(newPrice)), at: self.budgetList.count)
-                print(#fileID, #function, #line, "- budgetList: \(self.budgetList)")
-                self.updateDailySpend()
-                self.dailyBudgetTableView.reloadData()
+        SwiftAlertView.show(title: "추가하시겠습니까?", message: "무엇에 얼마를 썼나요?", buttonTitles: "취소", "추가") { alertView in
+            alertView.addTextField { textField in
+                textField.placeholder = "내역"
+                textField.tintColor = .primaryColor ?? .black
             }
-        })
-        alertController.addAction(confirmAction)
+            alertView.addTextField { textField in
+                textField.placeholder = "금액"
+                textField.keyboardType = .numberPad
+                textField.tintColor = .primaryColor ?? .black
+                textField.delegate = self
+            }
+            alertView.isEnabledValidationLabel = true
+            alertView.isDismissOnActionButtonClicked = false
+            alertView.backgroundColor = .backgroundColor
+            alertView.buttonTitleColor = .primaryColor ?? .blue
+        }
+        .onActionButtonClicked { alertView, buttonIndex in
+            
+            guard let breakdown = alertView.textField(at: 0)?.text,
+                  let amountOfMoney = alertView.textField(at: 1)?.text else { return }
+    
+            switch buttonIndex {
+            case 0:
+                print(#fileID, #function, #line, "- cancel btn clicked")
+            case 1:
+                print(#fileID, #function, #line, "- ok btn clicked")
+                if breakdown.isEmpty {
+                    alertView.validationLabel.text = "내역이 비어있습니다."
+                } else if amountOfMoney.isEmpty {
+                    alertView.validationLabel.text = "금액을 입력해주세요"
+                } else {
+                    DispatchQueue.main.async {
+                        self.budgetList.insert(Budget(title: breakdown, price: Int(amountOfMoney)), at: self.budgetList.count)
+                        print(#fileID, #function, #line, "- budgetList: \(self.budgetList)")
+                        self.updateDailySpend()
+                        self.dailyBudgetTableView.reloadData()
+                    }
+                    alertView.dismiss()
+                }
+                
+            default:
+                print(#fileID, #function, #line, "- ")
+            }
+        }
+        .onTextChanged { _, text, textFieldIndex in
+            if textFieldIndex == 0 {
+                print("Username text changed: ", text ?? "")
+            }
+        }
         
-        // Alert action cancel
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: {(_ action: UIAlertAction) -> Void in
-            print("취소")
-        })
-        alertController.addAction(cancelAction)
         
-        // Present alert controller
-        present(alertController, animated: true, completion: nil)
+//        let alertController = UIAlertController(title: "추가하시겠습니까?", message: "무엇에 얼마를 썼나요?", preferredStyle: .alert)
+//        
+//        
+//        // add textfield at index 0
+//        alertController.addTextField(configurationHandler: {(_ textField: UITextField) -> Void in
+//            textField.placeholder = "내역"
+//            
+//        })
+//        
+//        // add textfield at index 1
+//        alertController.addTextField(configurationHandler: {(_ textField: UITextField) -> Void in
+//            textField.placeholder = "금액"
+//            textField.keyboardType = .numberPad
+//            textField.delegate = self
+//        })
+//        
+//        // Alert action confirm
+//        let confirmAction = UIAlertAction(title: "OK", style: .default, handler: {(_ action: UIAlertAction) -> Void in
+//            
+//            guard let newTitle = alertController.textFields?[0].text,
+//                  let newPrice = alertController.textFields?[1].text else { return }
+//            
+//            
+//            print("내역: \(newTitle)")
+//            print("금액: \(newPrice)")
+//            
+//            DispatchQueue.main.async {
+//                self.budgetList.insert(Budget(title: newTitle, price: Int(newPrice)), at: self.budgetList.count)
+//                print(#fileID, #function, #line, "- budgetList: \(self.budgetList)")
+//                self.updateDailySpend()
+//                self.dailyBudgetTableView.reloadData()
+//            }
+//        })
+//        alertController.addAction(confirmAction)
+//        
+//        // Alert action cancel
+//        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: {(_ action: UIAlertAction) -> Void in
+//            print("취소")
+//        })
+//        alertController.addAction(cancelAction)
+//        
+//        // Present alert controller
+//        present(alertController, animated: true, completion: nil)
     }
     
     @IBAction func submitBtnClicked(_ sender: UIButton) {
@@ -146,21 +198,31 @@ class DailyBudgetVC: UIViewController {
         // 1. tableView에 아무 내역도 없으면 경고 얼럿 띄우기
         if self.budgetList.isEmpty {
             // 경고 얼럿 화면 띄우기
-            let alert = UIAlertController(title: "내역 입력 없이 돌아가시겠습니까?", message: "오늘의 소비를 입력하지 않았습니다.", preferredStyle: .alert)
-            let cancelAction = UIAlertAction(title: "입력하기", style: .cancel, handler: {(_ action: UIAlertAction) -> Void in
-                print("돌아가서 입력하기")
+            SwiftAlertView.show(title: "내역 입력 없이 돌아가시겠습니까?", message: "오늘의 소비를 입력하지 않았습니다.", buttonTitles: "메인으로", "입력하기") { alertView in
+                alertView.backgroundColor = .backgroundColor
+                //            alertView.backgroundColor = UIColor(named: "redBean")
+                alertView.cancelButtonIndex = 1
+                alertView.buttonTitleColor = .primaryColor ?? .black
+                alertView.transitionType = .fade
+            }
+            .onButtonClicked({ alertView, buttonIndex in
+                switch buttonIndex {
+                case 0:
+                    print(#fileID, #function, #line, "- cancel btn clicked")
+                    self.navigationController?.popViewController(animated: true)
+                case 1:
+                    print(#fileID, #function, #line, "- ok btn clicked")
+                    alertView.dismiss()
+                default:
+                    print(#fileID, #function, #line, "- ")
+                }
             })
-            alert.addAction(cancelAction)
-            let confirmAction = UIAlertAction(title: "메인으로", style: .default, handler: { _ in
-                print("메인으로 돌아가기")
-                self.navigationController?.popViewController(animated: true)
-            })
-            alert.addAction(confirmAction)
-            self.present(alert, animated: true, completion: nil)
+            
         } else {
             // 2. DailyMain 화면으로 돌아가기
             self.navigationController?.popViewController(animated: true)
         }
+        
     }
     
     
---
+++ Ulmago/Extension/UIColor+Ext.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Ext.swift
+//  Ulmago
+//
+//  Created by dhui on 3/25/24.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIColor {
+    static let backgroundColor = UIColor(named: "backgroundColor")
+    static let primaryColor = UIColor(named: "redBean")
+}
